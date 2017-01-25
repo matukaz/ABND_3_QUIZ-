4948820e1fc0f0204b06ced0db4752b742c86f84
Matu <matukazz@hotmail.com>
2017-01-25 23:07:25 +0200

    init commit

---
+++ app/src/main/java/com/teddydev/abnd_3_quiz_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.teddydev.abnd_3_quiz_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
